7b420c01683c293940a91fc49a3a8e5a531bcf5c
Theo Vora <theo.vora@gmail.com>
2020-03-09 17:20:41 -0600

    complete playground. will return person's films.

--- SWAPI.playground/Contents.swift
+++ SWAPI.playground/Contents.swift
@@ -12,7 +12,7 @@ struct Person: Decodable {
     let birth_year: String
     let gender: String
     let homeworld: String
-    let films: [String]
+    let films: [URL]
     let species: [String]
     let vehicles: [String]
     let starships: [String]
@@ -33,6 +33,7 @@ struct Film: Decodable {
 class SwapiService {
     static private let baseURL = URL(string: "https://swapi.co/api/")
     static private let peopleEndpoint = "people"
+    static private let filmsEndpoint = "films"
     
     static func fetchPerson(id: Int, completion: @escaping (Person?) -> Void) {
         // 1 - Prepare URL
@@ -71,17 +72,48 @@ class SwapiService {
     static func fetchFilm(url: URL, completion: @escaping (Film?) -> Void) {
         // 1 - Contact server
         
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            // 2 - Handle errors
+
+            if let error = error {
+                print(error, error.localizedDescription)
+                return completion(nil)
+            }
+            
+            // 3 - Check for data
+            guard let data = data else { return completion(nil) }
+            
+            // 4 - Decode Film from JSON
+            do {
+                let film = try JSONDecoder().decode(Film.self, from: data)
+                return completion(film)
+            } catch {
+                print(error, error.localizedDescription)
+                return completion(nil)
+            }
+        }.resume()
         
-        // 2 - Handle errors
         
-        // 3 - Check for data
         
-        // 4 - Decode Film from JSON
+    }
+}
+
+func fetchFilm(url: URL) {
+    SwapiService.fetchFilm(url: url) { film in
+        if let film = film {
+            print(film.title)
+        }
     }
 }
 
 SwapiService.fetchPerson(id: 10) { person in
     if let person = person {
         print(person)
+        
+        let films = person.films
+        print("Films in which \(person) appears:")
+        for film in films {
+            fetchFilm(url: film)
+        }
     }
 }
